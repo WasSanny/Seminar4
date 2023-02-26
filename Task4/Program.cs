cdb256d81b147ce12178035760a93b2bd84c56b2
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-26 21:45:01 +0300

    Выводим длину числа(количество символов)

---
+++ Task4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа, которая принмает на вход число,
+// и выдает количество чисел входящего числа
+
+Console.WriteLine(CalculateNumberToNumber(ReadInt("number")));
+
+int ReadInt(string argument)  // функция проверяет ввели ли мы целое число до тех пор,
+{                              // пока мы не введем целове число
+  Console.Write($"Input {argument}: ");
+  int number;
+
+  while(!int.TryParse(Console.ReadLine(), out number))
+  {
+    Console.WriteLine("Вы ввели не число!");
+  }
+
+  return number;
+}
+
+int CalculateNumberToNumber(int numb)
+{
+  int length = numb.ToString().Length;
+  return length;
+}
+
