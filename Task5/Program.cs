9fcf8abb93bf80b4d14a2909acf5d237f22ad35c
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-26 22:07:16 +0300

    Считаем произведение от 1 до (принимаемого на вход числа)

---
+++ Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая принимает на вход число (N), и
+// выдает произведение от 1 до (N)
+
+Console.WriteLine(CalculateResaltToN(ReadInt("number")));
+
+int ReadInt(string argument)  // функция проверяет ввели ли мы целое число до тех пор,
+{                              // пока мы не введем целове число
+  Console.Write($"Input {argument}: ");
+  int number;
+
+  while(!int.TryParse(Console.ReadLine(), out number))
+  {
+    Console.WriteLine("Вы ввели не число!");
+  }
+
+  return number;
+}
+
+int CalculateResaltToN(int number)
+{
+  int result = 1;
+
+  for(int i = 1; i <= number; i++)
+  {
+    result = result * i;
+  }
+
+  return result;
+}
