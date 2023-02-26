93f7c9939575e0d4e163a85cd2074a92b7a4387c
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-26 22:47:05 +0300

    Выводим массив заполненный 0 и 1 в случ порядке

---
+++ Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая выводит массив из 8 элементов,
+// заполненный нулями и еденицами в случайном порядке!
+
+// Random random = new Random();   /// Через цикл While
+// int min = 0;
+// int max = 2;
+// int index = 0;
+// int[] array = new int[8];
+
+// while(index < 8)
+// {
+//   array[index] = random.Next(min, max);
+//   Console.Write(array[index]);
+//   index++;
+// }
+
+Random random = new Random();   /// Через цикл for
+int min = 0;
+int max = 2;
+int[] array = new int[8];
+
+for (int index = 0; index < 8; index++)
+{
+  array[index] = random.Next(min, max);
+  Console.Write(array[index]);
+}
